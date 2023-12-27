5e0307da80230165156a155f5e43cdaf9feb383d
nitinsahu19 <sahunitin900@gmail.com>
2023-12-27 19:39:34 +0530

    integrated expenses with server

--- src/components/pages/Expenses.js
+++ src/components/pages/Expenses.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./Expenses.module.css";
 
 const Expenses = () => {
@@ -13,10 +13,36 @@ const Expenses = () => {
     event.preventDefault();
 
     // Create a new expense object with the form input values
-    const newExpense = { amount, description, category, customCategory };
+    // const newExpense = { amount, description, category, customCategory };
+    const newExpense = {
+      amount,
+      description,
+      category,
+    };
+    // condtionally sote custom category in objectx`
+    if (customCategory) {
+      newExpense.customCategory = customCategory;
+    }
 
     // Update the expenses state with the new expense using the spread operator
-    setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+    // setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+
+    fetch(
+      "https://expense-tracker-56d18-default-rtdb.firebaseio.com/expenses.json",
+      {
+        method: "POST",
+        body: JSON.stringify(newExpense),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Adding new expense failed");
+        }
+        return response.json();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error.message));
 
     // Clear form input values after submission
     setAmount("");
@@ -24,6 +50,34 @@ const Expenses = () => {
     setCategory("");
     setCustomCategory("");
   };
+
+  // Use the useEffect hook to perform an asynchronous operation when the component mounts
+  useEffect(() => {
+    // Make a GET request to the specified API endpoint
+    fetch(
+      "https://expense-tracker-56d18-default-rtdb.firebaseio.com/expenses.json",
+      { method: "GET" }
+    )
+      // Handle the response from the API
+      .then((response) => {
+        // Check if the response status is okay
+        if (!response.ok) {
+          // If not okay, throw an error with a custom message
+          throw new Error("Data fetching failed");
+        }
+        // If okay, parse the response body as JSON and return the result
+        return response.json();
+      })
+      // Handle the parsed data from the JSON response
+      .then((data) => {
+        // Extract values from the data object (or use an empty object if data is null or undefined)
+        const fetchedExpenses = Object.values(data || {});
+        // Set the component's state with the fetched expenses
+        setExpenses(fetchedExpenses);
+      })
+      // Handle any errors that occurred during the fetch or parsing process
+      .catch((error) => console.log(error.message));
+  }, []); // The empty dependency array ensures that this useEffect runs only once when the component mounts
 
   return (
     <>
@@ -81,21 +135,21 @@ const Expenses = () => {
         {/* Submit Button */}
         <button type="submit">Add Expense</button>
 
-        {/* Display Expenses List */}
-        <section>
-          {/* Display expenses using the map function */}
-          {expenses.length > 0 && (
-            <ul className={classes.expenses_list}>
-              {expenses.map((item, index) => (
-                <li key={index}>
-                  {item.description}: {item.amount}, {item.category}
-                  {/* Display custom category in parentheses for "Other" category */}
-                  {item.category === "Other" && `(${item.customCategory})`}
-                </li>
-              ))}
-            </ul>
-          )}
-        </section>
+        {/* Display expenses using the map function */}
+        {expenses.length > 0 && (
+          <ul className={classes.expenses_list}>
+            {expenses.map((item, index) => (
+              <li key={index}>
+                <strong>Item:</strong>
+                {item.description}: <strong>Rs.</strong>
+                {item.amount}, <strong>Category:</strong>
+                {item.category}
+                {/* Display custom category in parentheses for "Other" category */}
+                {item.category === "Other" && `(${item.customCategory})`}
+              </li>
+            ))}
+          </ul>
+        )}
       </form>
     </>
   );
